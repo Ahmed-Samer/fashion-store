bc2bf75f98d3f8b8a184b07e6e4e9f70a80472c0
ahmedsamerkamal <ahmedsamer9746313@gmail.com>
2025-11-24 07:40:57 +0200

    Fix chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // 1. زودنا الحد المسموح للتحذير لـ 1.5 ميجا عشان فايربيز تقيل شوية
+    chunkSizeWarningLimit: 1600,
+    
+    // 2. إعدادات تقسيم الملفات (اختياري لتحسين الأداء)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          // بنفصل مكتبات node_modules في ملف لوحدها
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
